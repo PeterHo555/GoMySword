d99265e7470c638362ba5ed2bc5f19e7e3d44d82
PeterHo <peter45402@163.com>
2020-12-09 21:32:39 +0800

    go递归

--- Sword07.go
+++ Sword07.go
@@ -6,6 +6,15 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-//func buildTree(preorder []int, inorder []int) *TreeNode {
-//
-//}
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	for i := range inorder {
+		if inorder[i] == preorder[0] {
+			return &TreeNode{
+				Val: preorder[0],
+				Left: buildTree(preorder[1:i+1], inorder[0:i]),
+				Right: buildTree(preorder[i+1:], inorder[i+1:]),
+			}
+		}
+	}
+	return nil
+}
